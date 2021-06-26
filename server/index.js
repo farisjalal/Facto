03e62c5e0b0f5f3461bf76f0e210cb6655c5562b
Ayush Abraham <ayushabraham01@gmail.com>
2021-06-26 16:47:09 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -20,6 +20,6 @@ res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 
-  app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-  });
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
